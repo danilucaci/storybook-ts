453ec410d322d03351535b19c0d8c930c0de39a4
Dani Lucaci <danilucaci@gmail.com>
2021-07-04 20:36:02 +0200

    wip

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -6,16 +6,22 @@ import Button, { ButtonProps } from "./Button";
 export default {
   component: Button,
   title: "Components/Button",
-  argTypes: {
-    onClick: { action: "clicked" },
-  },
 } as Meta;
 
 const Template: Story<ButtonProps> = (args: ButtonProps) => (
   <Button {...args} />
 );
 
-export const Default: Story<ButtonProps> = Template.bind({});
-Default.args = {
-  label: "Open",
+export const Primary: Story<ButtonProps> = Template.bind({});
+Primary.args = {
+  label: "Click",
+  primary: true,
+  htmlType: "button",
+};
+
+export const Outlined: Story<ButtonProps> = Template.bind({});
+Outlined.args = {
+  label: "Click",
+  outlined: true,
+  htmlType: "button",
 };
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,23 +1,92 @@
 import * as React from "react";
-import styled from "styled-components";
+import styled, { DefaultTheme } from "styled-components";
 
-const StyledButton = styled.button`
-  padding-top: ${({ theme }) => theme.spacing.ui.button.s};
-  padding-bottom: ${({ theme }) => theme.spacing.ui.button.s};
-  padding-left: ${({ theme }) => theme.spacing.ui.button.s};
-  padding-right: ${({ theme }) => theme.spacing.ui.button.s};
-  color: ${({ theme }) => theme.colors.grey.grey900};
-  border: 1px solid ${({ theme }) => theme.colors.grey.grey900};
-  background-color: ${({ theme }) => theme.colors.grey.grey50};
-`;
-
-export interface ButtonProps {
+export type BaseButtonProps = {
   onClick: () => void;
   label: string;
+  htmlType: "button" | "submit";
+  className?: string;
+};
+
+export type PrimaryButtonProps = BaseButtonProps & {
+  primary: boolean;
+  outlined: never;
+};
+
+export type OutlinedButtonProps = BaseButtonProps & {
+  primary: never;
+  outlined: boolean;
+};
+
+export type ButtonProps = PrimaryButtonProps | OutlinedButtonProps;
+
+export type StyledButtonProps = BaseButtonProps & {
+  theme: DefaultTheme;
+  primary?: boolean;
+  outlined?: boolean;
+};
+
+function backgroundColorSelector({
+  primary,
+  outlined,
+  theme,
+}: StyledButtonProps): string {
+  if (outlined) {
+    return theme.colors.grey.grey50;
+  }
+  if (primary) {
+    return theme.colors.primary.primary50;
+  }
+
+  return theme.colors.grey.grey50;
 }
 
-const Button: React.FC<ButtonProps> = ({ label, onClick }: ButtonProps) => {
-  return <StyledButton onClick={onClick}>{label}</StyledButton>;
+function colorSelector({
+  primary,
+  outlined,
+  theme,
+}: StyledButtonProps): string {
+  if (outlined) {
+    return theme.colors.grey.grey900;
+  }
+  if (primary) {
+    return theme.colors.primary.primary600;
+  }
+
+  return theme.colors.grey.grey900;
+}
+
+const StyledButton = styled.button<StyledButtonProps>`
+  padding-top: ${({ theme }) => theme.spacing.ui.button.s};
+  padding-bottom: ${({ theme }) => theme.spacing.ui.button.s};
+  padding-left: ${({ theme }) => theme.spacing.ui.button.sm};
+  padding-right: ${({ theme }) => theme.spacing.ui.button.sm};
+  color: ${colorSelector};
+  border: 1px solid ${colorSelector};
+  background-color: ${backgroundColorSelector};
+`;
+
+const Button: React.FC<ButtonProps> = ({
+  onClick,
+  label,
+  htmlType,
+  primary,
+  outlined,
+}: ButtonProps) => {
+  return (
+    <StyledButton
+      primary={primary}
+      outlined={outlined}
+      type={htmlType}
+      onClick={onClick}
+    >
+      {label}
+    </StyledButton>
+  );
 };
+
+function App() {
+  return <Button outlined onClick={() => {}} htmlType="button" label="hola" />;
+}
 
 export default Button;
